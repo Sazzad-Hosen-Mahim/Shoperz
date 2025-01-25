433cdd0efea29747faf22def1a3a6e3fbe083fe8
Abu Hossain Foysal <ahfoysal30@gmail.com>
2025-01-25 18:28:50 +0600

    sadas

--- src/components/CommonWrapper.jsx
+++ src/components/CommonWrapper.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 
-const CommonWrapper = ({children}) => {
+const CommonWrapper = ({children, className}) => {
   return (
-    <div className="max-w-[1400px] py-4 mx-auto my-auto">
+    <div className={`max-w-[1400px] py-4 mx-auto my-auto ${className}`}>
       {children}
     </div>
   )
