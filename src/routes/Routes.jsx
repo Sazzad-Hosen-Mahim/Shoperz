5960a7e30829f501f32c92a050ad01838b4fe6ce
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-02-11 16:41:47 +0600

    route set up

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,6 +4,14 @@ import App from "../App";
 import NotFound from "../pages/NotFound";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import NewArrival from "../pages/NewArrival";
+import MyCloset from "../pages/MyCloset";
+import Shop from "../pages/Shop";
+import Contact from "../pages/Contact";
+import Selling from "../pages/Selling";
+import ShoppingCart from "../pages/ShoppingCart";
+import Checkout from "../pages/Checkout";
+import Profile from "../pages/Profile";
 
 const routes = createBrowserRouter([
   {
@@ -16,14 +24,48 @@ const routes = createBrowserRouter([
         element: <Home />,
       },
       {
-        index: true,
         path: "/login",
-        element: <SignIn /> ,
+        element: <SignIn />,
       },
       {
-        index: true,
         path: "/signup",
         element: <SignUp />,
+      },
+      {
+        path: "/closet",
+        element: <MyCloset />,
+      },
+      {
+        path: "/new-arrival",
+        element: <NewArrival />,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/selling",
+        element: <Selling />,
+      },
+      {
+        path: "/product-details",
+        element: <ProductDetails />,
+      },
+      {
+        path: "/shopping-cart",
+        element: <ShoppingCart />,
+      },
+      {
+        path: "/checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
       },
     ],
   },
