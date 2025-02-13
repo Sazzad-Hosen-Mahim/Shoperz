18e36b0a4428a2671af5038377b6dbb1bb1405a7
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-02-13 16:34:55 +0600

    new arrival in homepage done

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -1,10 +1,12 @@
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import CommonWrapper from "../CommonWrapper";
+import CollectionCard from "./CollectionCard";
+import { collectionProducts } from "../../lib/data";
 
 const Collection = () => {
   return (
-    <div>
+    <div className="mt-[140px]">
       <CommonWrapper>
         <div>
           <RadioGroup
@@ -17,7 +19,9 @@ const Collection = () => {
                 id="option-one"
                 className="text-black border-black"
               />
-              <Label htmlFor="option-one text-geist ">All Collection</Label>
+              <Label htmlFor="option-one" className="text-[48px] font-[600]">
+                All Collection
+              </Label>
             </div>
             <div className="flex items-center space-x-2">
               <RadioGroupItem
@@ -25,7 +29,12 @@ const Collection = () => {
                 id="option-two"
                 className="text-black border-black"
               />
-              <Label htmlFor="option-two text-geist">New Collection</Label>
+              <Label
+                htmlFor="option-two text-geist"
+                className="text-[48px] font-[600] "
+              >
+                New Collection
+              </Label>
             </div>
             <div className="flex items-center space-x-2">
               <RadioGroupItem
@@ -33,9 +42,19 @@ const Collection = () => {
                 id="option-three"
                 className="text-black border-black"
               />
-              <Label htmlFor="option-three text-geist">What&apos;s Hot</Label>
+              <Label
+                htmlFor="option-three text-geist"
+                className="text-[48px] font-[600]"
+              >
+                What&apos;s Hot
+              </Label>
             </div>
           </RadioGroup>
+        </div>
+        <div className="grid grid-cols-3 gap-[10px] mt-[75px]">
+          {collectionProducts.map((card, i) => (
+            <CollectionCard card={card} key={i} />
+          ))}
         </div>
       </CommonWrapper>
     </div>
