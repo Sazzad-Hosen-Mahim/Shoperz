705c852dcb5ec916ff47fe7e809ba414527334b2
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-02-13 14:04:15 +0600

    layout and footer issue fixed

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -6,7 +6,7 @@ export default function MainLayout() {
   return (
     <div>
       <Header />
-      <div className="min-h-screen py-20 flex max-h-full flex-nowrap justify-start  transition-[padding] duration-200 p-0">
+      <div className="min-h-screen py-20 flex max-h-full flex-nowrap justify-start bg-gradient-to-r from-[#F1FBFF] via-[#F1EDEB] to-[#F8DAB0] transition-[padding] duration-200 p-0">
         <div className="w-full">
           <Outlet />
         </div>
